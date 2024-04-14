0636ed0ecb68b6bea0c99ad2f9270359df906f3d
Szabó Tímea <timeaszabo1217@gmail.com>
2024-04-14 21:32:04 +0200

    favicon html

--- src/connect.php
+++ src/connect.php
@@ -9,6 +9,7 @@ include 'conn.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
     <title>𝐒𝐭𝐫𝐞𝐞𝐥𝐞𝐫</title>
 </head>
 <body>
