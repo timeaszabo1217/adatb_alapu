aabd32311a405c1c4573d45113fad970aac2e0ae
Szabó Tímea <timeaszabo1217@gmail.com>
2024-04-14 19:25:32 +0200

    menusor

--- src/conn.php
+++ src/conn.php
@@ -6,12 +6,3 @@ $username = '';
 $password = '';
 
 $connection = oci_connect($username, $password, "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = $host)(PORT = $port)))(CONNECT_DATA=(SID=$db_service_name)))");
-
-if ($connection) {
-    echo "<p>Sikeresen csatlakozva az Oracle adatbázishoz.</p>";
-} else {
-    $error = oci_error();
-    echo "<p>Hiba a kapcsolódás során: " . $error['message'] . "</p>";
-}
-
-oci_close($connection);
