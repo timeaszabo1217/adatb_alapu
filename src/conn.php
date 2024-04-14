a8432485b4879c4fc0fff7d42f499be317c1e927
Szabó Tímea <timeaszabo1217@gmail.com>
2024-04-14 18:51:05 +0200

    connect oldal

--- src/conn.php
+++ src/conn.php
@@ -1,13 +1,17 @@
 <?php
-
 $host = 'localhost';
 $port = '1521';
 $db_service_name = 'orania2';
-$username = ''; // saját username
-$password = ''; // saját password
+$username = '';
+$password = '';
 
 $connection = oci_connect($username, $password, "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = $host)(PORT = $port)))(CONNECT_DATA=(SID=$db_service_name)))");
 
 if ($connection) {
-    echo "Sikeresen csatlakozva az Oracle adatbázishoz.\n";
+    echo "<p>Sikeresen csatlakozva az Oracle adatbázishoz.</p>";
+} else {
+    $error = oci_error();
+    echo "<p>Hiba a kapcsolódás során: " . $error['message'] . "</p>";
 }
+
+oci_close($connection);
---
+++ src/connect.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>𝐒𝐭𝐫𝐞𝐞𝐥𝐞𝐫</title>
+</head>
+<body>
+<div class="container">
+    <h1>Oracle adatbázis kapcsolódás</h1>
+
+    <?php include 'conn.php'; ?>
+
+    <img src="assets/imgs/240-2401373_book-free-download-best-magic-spell-book-clipart-removebg-preview.png" alt="opened book graphic">
+
+    <a href="index.php" class="continueButoon">Tovább az oldalra</a>
+</div>
+</body>
+</html>
