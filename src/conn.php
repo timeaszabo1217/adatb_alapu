95d1d3aa26236b12f63cb12febc1c5b1f9ee133e
Szabó Tímea <timeaszabo1217@gmail.com>
2024-04-14 14:45:27 +0200

    Update conn.php

--- src/conn.php
+++ src/conn.php
@@ -3,8 +3,8 @@
 $host = 'localhost';
 $port = '1521';
 $db_service_name = 'orania2';
-$username = 'C##JJMS9M';
-$password = 'C##JJMS9M';
+$username = ''; // saját username
+$password = ''; // saját password
 
 $connection = oci_connect($username, $password, "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = $host)(PORT = $port)))(CONNECT_DATA=(SID=$db_service_name)))");
 
