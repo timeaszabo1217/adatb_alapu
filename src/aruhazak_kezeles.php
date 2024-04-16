303fd21b77c9b57c954233fb4de9d3de0cada2d9
Szabó Tímea <timeaszabo1217@gmail.com>
2024-04-16 21:25:46 +0200

    aruhaz
    módosítás itt se 100as

--- src/aruhazak_kezeles.php
+++ src/aruhazak_kezeles.php
@@ -12,6 +12,72 @@ if (isset($_POST["aruhaz_delete"])) {
     header("Location: aruhazak_kezeles.php");
     exit();
 }
+
+if (isset($_POST["aruhaz_add"])) {
+    $iranyitoszam = $_POST["iranyitoszam_add"];
+    $varos = $_POST["varos_add"];
+    $utca = $_POST["utca_add"];
+    $hazszam = $_POST["hazszam_add"];
+    $dolgozok_szama = $_POST["dolgozok_szama_add"];
+
+    $stid = oci_parse(database(),
+        "INSERT INTO ARUHAZ (IRANYITOSZAM, VAROS, UTCA, HAZSZAM, DOLGOZOK_SZAMA)
+         VALUES (:iranyitoszam, :varos, :utca, :hazszam, :dolgozok_szama)");
+
+    oci_bind_by_name($stid, ":iranyitoszam", $iranyitoszam);
+    oci_bind_by_name($stid, ":varos", $varos);
+    oci_bind_by_name($stid, ":utca", $utca);
+    oci_bind_by_name($stid, ":hazszam", $hazszam);
+    oci_bind_by_name($stid, ":dolgozok_szama", $dolgozok_szama);
+
+    oci_execute($stid);
+
+    header("Location: aruhazak_kezeles.php");
+}
+
+if (isset($_POST["aruhaz_modify"])) {
+    if (isset($_POST["aruhaz_id"])) {
+        $aruhaz_id = $_POST["aruhaz_id"];
+        $query = "UPDATE Aruhaz SET ";
+        $update_values = array();
+
+        if (!empty($_POST["iranyitoszam_modify"])) {
+            $query .= "IRANYITOSZAM = :iranyitoszam, ";
+            $update_values[':iranyitoszam'] = $_POST["iranyitoszam_modify"];
+        }
+        if (!empty($_POST["varos_modify"])) {
+            $query .= "VAROS = :varos, ";
+            $update_values[':varos'] = $_POST["varos_modify"];
+        }
+        if (!empty($_POST["utca_modify"])) {
+            $query .= "UTCA = :utca, ";
+            $update_values[':utca'] = $_POST["utca_modify"];
+        }
+        if (!empty($_POST["hazszam_modify"])) {
+            $query .= "HAZSZAM = :hazszam, ";
+            $update_values[':hazszam'] = $_POST["hazszam_modify"];
+        }
+        if (!empty($_POST["dolgozok_szama_modify"])) {
+            $query .= "DOLGOZOK_SZAMA = :dolgozok_szama, ";
+            $update_values[':dolgozok_szama'] = $_POST["dolgozok_szama_modify"];
+        }
+
+        $query = rtrim($query, ", ");
+        $query .= " WHERE ARUHAZ_ID = :aruhaz_id";
+
+        $stid = oci_parse(database(), $query);
+
+        foreach ($update_values as $key => &$value) {
+            oci_bind_by_name($stid, $key, $value);
+        }
+        oci_bind_by_name($stid, ':aruhaz_id', $aruhaz_id);
+        oci_execute($stid);
+
+        header("Location: aruhazak_kezeles.php");
+        exit();
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -66,6 +132,76 @@ if (isset($_POST["aruhaz_delete"])) {
                 </tbody>
             </table>
         </section>
+        </form>
+    </div>
+    <div class="book-form-container">
+        <h2>Áruház hozzáadása</h2>
+        <form method="POST" action="aruhazak_kezeles.php" accept-charset="utf-8">
+            <div class="input-container">
+                <label for="iranyitoszam">Irányítószám:</label>
+                <label>
+                    <input type="text" name="iranyitoszam_add"/>
+                </label>
+                <label for="varos">Város:</label>
+                <label>
+                    <input type="text" name="varos_add"/>
+                </label>
+                <label for="utca">Utca:</label>
+                <label>
+                    <input type="text" name="utca_add"/>
+                </label>
+                <label for="hazszam">Házszám:</label>
+                <label>
+                    <input type="text" name="hazszam_add"/>
+                </label>
+                <label for="dolgozok_szama">Dolgozók száma:</label>
+                <label>
+                    <input type="text" name="dolgozok_szama_add"/>
+                </label>
+            </div>
+            <input class="continueButton" type="submit" name="aruhaz_add" value="Hozzáadás" />
+        </form>
+    </div>
+    <div class="book-form-container">
+        <h2>Áruház módosítása</h2>
+        <form method="POST" action="aruhazak_kezeles.php" accept-charset="utf-8">
+            <div class="select-container">
+                <p>Válassz az áruházak listából, ha módosítani szeretnél:</p>
+                <label>
+                    <select name="aruhaz_id">
+                        <?php
+                        $stid = oci_parse(database(), 'SELECT ARUHAZ_ID, IRANYITOSZAM, VAROS, UTCA, HAZSZAM, DOLGOZOK_SZAMA FROM Aruhaz');
+                        oci_execute($stid);
+                        while (($row = oci_fetch_row($stid)) != false) {
+                            echo '<option value="' . $row[0] . '">' . $row[1] . ' - ' . $row[2] . ' - ' . $row[3] . ' - ' . $row[4] . ' - ' . $row[5] . '</option>';
+                        }
+                        ?>
+                    </select>
+                </label>
+            </div>
+            <div class="input-container">
+                <label for="iranyitoszam_modify">Irányítószám:</label>
+                <label>
+                    <input type="text" name="iranyitoszam_modify"/>
+                </label>
+                <label for="varos_modify">Város:</label>
+                <label>
+                    <input type="text" name="varos_modify"/>
+                </label>
+                <label for="utca_modify">Utca:</label>
+                <label>
+                    <input type="text" name="utca_modify"/>
+                </label>
+                <label for="hazszam_modify">Házszám:</label>
+                <label>
+                    <input type="text" name="hazszam_modify"/>
+                </label>
+                <label for="dolgozok_szama_modify">Dolgozók száma:</label>
+                <label>
+                    <input type="text" name="dolgozok_szama_modify"/>
+                </label>
+            </div>
+            <input class="continueButton" type="submit" name="aruhaz_modify" value="Módosítás" />
         </form>
     </div>
 </main>
