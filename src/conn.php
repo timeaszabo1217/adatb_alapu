bb0c7037fe1b20bb87d7e815a2e4eae4b9169ed3
Szabó Tímea <timeaszabo1217@gmail.com>
2024-04-17 20:08:13 +0200

    Update conn.php

--- src/conn.php
+++ src/conn.php
@@ -4,8 +4,8 @@ global $connection;
 $host = 'localhost';
 $port = '1521';
 $db_service_name = 'orania2';
-$username = '';
-$password = '';
+$username = 'C##E1T43H';
+$password = 'Goffry2019';
 
 $connection = oci_connect(
     $username,
