c4931f365e64d15628eee1fc3c238fccad6b0055
Szabó Tímea <timeaszabo1217@gmail.com>
2024-04-17 19:21:28 +0200

    ékezetes probléma winwin

--- src/conn.php
+++ src/conn.php
@@ -4,7 +4,12 @@ global $connection;
 $host = 'localhost';
 $port = '1521';
 $db_service_name = 'orania2';
-$username = 'C##E1T43H';
-$password = 'Goffry2019';
+$username = ''; // saját név
+$password = ''; // saját jelszó
 
-$connection = oci_connect($username, $password, "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = $host)(PORT = $port)))(CONNECT_DATA=(SID=$db_service_name)))");
+$connection = oci_connect(
+    $username,
+    $password,
+    "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = $host)(PORT = $port)))(CONNECT_DATA=(SID=$db_service_name)))",
+    'AL32UTF8'
+);
