f22056ed5388a8cace0bf007f2bd10fd4951b681
Szabó Tímea <timeaszabo1217@gmail.com>
2024-04-17 16:02:26 +0200

    betűtípus import

--- src/konyvek_kezeles.php
+++ src/konyvek_kezeles.php
@@ -216,14 +216,14 @@ if (isset($_POST['almufaj_delete'])) {
 <h1 style="text-align: center">Admin oldal</h1>
 <div class="menu">
     <ul>
-        <li><a href="fiok_kezeles.php">Fiókok kezelése</a></li>
-        <li><a href="konyvek_kezeles.php">Könyvek kezelése</a></li>
-        <li><a href="aruhazak_kezeles.php">Áruházak kezelése</a></li>
+        <li><img class="listajel" src="assets/imgs/icon.png" alt="listajel"><a href="fiok_kezeles.php">Fiókok kezelése</a></li>
+        <li><img class="listajel" src="assets/imgs/icon.png" alt="listajel"><a href="konyvek_kezeles.php">Könyvek kezelése</a></li>
+        <li><img class="listajel" src="assets/imgs/icon.png" alt="listajel"><a href="aruhazak_kezeles.php">Áruházak kezelése</a></li>
     </ul>
 </div>
 <main>
     <div class="book-form-container">
-        <h2>Könyvek törlése</h2>
+        <h2>Könyvek törlése123</h2>
         <form method="POST" action="konyvek_kezeles.php" accept-charset="utf-8">
             <section>
                 <table>
