06e3e4dc7572d9b061129ec25fda3a15ff27cd04
Szabó Tímea <timeaszabo1217@gmail.com>
2024-04-28 21:20:08 +0200

    footer (libris oldalakkal)
    Később lehet vele mókolni, a boltkereső érdekes funkció lenne kidolgozásra, stb.

---
+++ src/footer.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <title>𝐒𝐭𝐫𝐞𝐞𝐥𝐞𝐫</title>
+</head>
+<body>
+<footer>
+    <div class="column">
+        <h3>Kövess minket</h3>
+        <div class="social-icons">
+            <a href="https://www.facebook.com/librikonyvesboltok/" target="_blank"><img src="assets/imgs/facebook-icon.png" alt="Facebook"></a>
+            <a href="https://www.youtube.com/channel/UCWmwcPyrGZnaGrgHNHVjx_Q" target="_blank"><img src="assets/imgs/youtube-icon.png" alt="Youtube"></a>
+            <a href="https://www.instagram.com/libri_konyvesboltok/" target="_blank"><img src="assets/imgs/instagram-icon.png" alt="Instagram"></a>
+            <a href="https://www.linkedin.com/company/libri-bookline/" target="_blank"><img src="assets/imgs/linkedin-icon.png" alt="Linkedin"></a>
+        </div>
+    </div>
+    <div class="column">
+        <h3>Streeler</h3>
+        <a href="https://www.libri.hu/rolunk" target="_blank">Rólunk</a>
+        <a href="https://libribookline.hrmaster.hu/DataCenter/Registration/JobAdvertisements/allasok" target="_blank">Karrier</a>
+        <a href="https://www.libri.hu/cikk/impresszum" target="_blank">Impresszum</a>
+        <a href="https://hexacafe.hu/itallap/" target="_blank">Kávézó</a>
+    </div>
+    <div class="column">
+        <h3>Szolgáltatás</h3>
+        <a href="https://www.libri.hu/boltok/" target="_blank">Boltkereső</a>
+        <a href="https://www.libri.hu/cikk/fizetes_es_szallitas" target="_blank">Fizetés és szállítás</a>
+        <a href="https://www.libri.hu/cikk/ugyfelszolgalat" target="_blank">Ügyfélszolgálat</a>
+    </div>
+    <div class="column">
+        <h3>Kultúra</h3>
+        <a href="https://www.libri.hu/esemenyek/" target="_blank">Események</a>
+    </div>
+    <div class="legal">
+        <a href="https://www.libri.hu/cikk/uzletszabalyzat" target="_blank">ÁSZF</a>
+        <a href="https://www.libri.hu/cikk/adatvedelem" target="_blank">Adatvédelem</a>
+        <p>© Streeler Könyvkereskedelmi Kft. Minden jog fenntartva!</p>
+    </div>
+</footer>
+</body>
+</html>
