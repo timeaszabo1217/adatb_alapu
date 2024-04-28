46256b52887712da4bea81333d198f35e30bcbee
Szabó Tímea <timeaszabo1217@gmail.com>
2024-04-29 01:36:27 +0200

    könyvek kis css

--- src/konyvek.php
+++ src/konyvek.php
@@ -15,25 +15,30 @@ include 'process.php';
 </head>
 <body>
 <h1>Könyvek</h1>
+<div class="book-form-container books-container">
+    <?php
+    $query = 'SELECT K.NEV, K.AR, KS.SZERZO FROM Konyv K INNER JOIN KonyvSzerzo KS ON K.Konyv_id = KS.Konyv_id';
+    $stid = oci_parse(database(), $query);
+    oci_execute($stid);
 
-<?php
-$query = 'SELECT K.NEV, K.AR, KS.SZERZO FROM Konyv K INNER JOIN KonyvSzerzo KS ON K.Konyv_id = KS.Konyv_id';
-$stid = oci_parse(database(), $query);
-oci_execute($stid);
-
-while ($row = oci_fetch_assoc($stid)) {
-    echo '<div>';
-    echo '<p>Cím: ' . $row['NEV'] . ', Szerző: ' . $row['SZERZO'] . ', Ár: ' . $row['AR'] . ' Ft</p>';
-    echo '<form method="post" action="kosar.php">';
-    echo '<input type="hidden" name="book_title" value="' . $row['NEV'] . '">';
-    echo '<input type="hidden" name="book_price" value="' . $row['AR'] . '">';
-    echo '<input type="submit" value="Kosárba">';
-    echo '</form>';
-    echo '</div>';
-}
-
-oci_free_statement($stid);
-oci_close($connection);
-?>
+    while ($row = oci_fetch_assoc($stid)) {
+        echo '<div style="display: flex; align-items: center;">';
+        echo '<img id="borito" src="assets/imgs/istockphoto-1132160175-612x612-removebg-preview.png" alt="Borítókép">';
+        echo '<div style="margin-left: 10px;">';
+        echo '<p>Cím: ' . $row['NEV'] . '</p>';
+        echo '<p>Szerző: ' . $row['SZERZO'] . '</p>';
+        echo '<p>Ár: ' . $row['AR'] . ' Ft</p>';
+        echo '</div>';
+        echo '<form class="basketButton" method="post" action="kosar.php">';
+        echo '<input type="hidden" name="book_title" value="' . $row['NEV'] . '">';
+        echo '<input type="hidden" name="book_price" value="' . $row['AR'] . '">';
+        echo '<input class="continueButton" type="submit" value="Kosárba">';
+        echo '</form>';
+        echo '</div>';
+    }
+    oci_free_statement($stid);
+    oci_close($connection);
+    ?>
+</div>
 </body>
 </html>
